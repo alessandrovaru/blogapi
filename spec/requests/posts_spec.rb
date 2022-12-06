2060c553f47d3435c8050c730a246be47257351a
Alessandrovaru <alessandrovaru@gmail.com>
2022-12-06 16:59:57 -0400

    Filtrar posts - pruebas

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,14 +5,27 @@ require "rails_helper"
 RSpec.describe "Posts", type: :request do
 
 	describe "GET /posts" do
-		before { get '/posts'}
 
 		it "should return OK" do
+			get '/posts'
 			payload = JSON.parse(response.body)
 			expect(payload).to be_empty
 			expect(response).to have_http_status(200)
 		end
 
+		describe "Search" do
+			let!(:hola_mundo) { create(:published_post, title: 'Hola mundo') }
+			let!(:hola_rails) { create(:published_post, title: 'Hola rails') }
+			let!(:hola_mail) { create(:published_post, title: 'Mail') }
+			it "should filter posts by title" do
+				get '/posts?search=Hola'
+				payload = JSON.parse(response.body)
+				expect(payload).to_not be_empty
+				expect(payload.size).to eq(2)
+				expect(payload..map{ |p| p["id"] }.sort).to eq([hola_mundo.id, hola_rails.id].sort)
+				expect(response).to have_http_status(200)
+			end
+		end
 	end
 
 	describe "with data in the DB" do

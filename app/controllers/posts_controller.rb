790a8a3b064866db5dd1d737292fbaff97e72f62
Alessandrovaru <alessandrovaru@gmail.com>
2022-12-10 13:48:17 -0400

    Finalizando pruebas de creación y actualización

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
-  before_action: :authenticate_user!, only [:create, :update]
+  before_action :authenticate_user!, only: [:create, :update]
 
-  rescue_from  Exception do |e|
+  rescue_from Exception do |e|
     render json: {error: e.message}, status: :internal_error
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: {error: e.message}, status: :not_found
   end
 
   rescue_from  ActiveRecord::RecordInvalid do |e|
@@ -19,16 +23,20 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
-    render json: @post, status: :ok
+    if(@post.published? || (Current.user && @post.user_id == Current.user.id))
+      render json: @post, status: :ok
+    else
+      render json: {error: 'Not Found'}, status: :not_found
+    end
   end
 
   def create
-    @post = Post.create!(create_params)
+    @post = Current.user.posts.create!(create_params)
     render json: @post, status: :created
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = Current.user.posts.find(params[:id])
     @post.update!(update_params)
     render json: @post, status: :ok
   end
@@ -36,7 +44,7 @@ class PostsController < ApplicationController
   private
 
   def create_params
-    params.require(:post).permit(:title, :content, :published, :user_id)
+    params.require(:post).permit(:title, :content, :published)
   end
 
   def update_params
@@ -48,7 +56,7 @@ class PostsController < ApplicationController
     token_regex = /Bearer (\w+)/
     #Leer el Header
     headers = request.headers
-    if headers['Authoraization'].present? && headers['Authorization'].match(token_regex)
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
       token = headers['Authorization'].match(token_regex)[1]
       if(Current.user = User.find_by_auth_token(token))
         return
